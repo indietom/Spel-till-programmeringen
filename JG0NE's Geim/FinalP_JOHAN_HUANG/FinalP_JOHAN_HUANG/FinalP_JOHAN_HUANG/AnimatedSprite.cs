c17e555a8b33bd49bf6091de8f7444a9538ab76c
jg0ne <johan_huang123@hotmail.com>
2014-05-16 20:00:26 +0200

    Le beta
    Final geim beta

---
+++ JG0NE's Geim/FinalP_JOHAN_HUANG/FinalP_JOHAN_HUANG/FinalP_JOHAN_HUANG/AnimatedSprite.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.Diagnostics;
+
+namespace FinalP_JOHAN_HUANG //your namespace goes here
+{
+    class AnimatedSprite
+    {
+        // Private members
+        private int columns;
+        private int rows;
+        private int currentColumn;
+        private int currentRow;
+        private int frameWidth;
+        private int frameHeight;
+        private int animationTimer;
+        private bool continuous;
+        private bool complete;
+
+        // Public properties
+        public Texture2D Texture { get; set; }
+        public int AnimationDelay { get; set; }
+        public bool Complete
+        {
+            get { return this.complete; } // This is made into a public property so that an object using the animated sprite can check if
+            // the animation is done and then react to that by changing its texture, removing itself etc.                                          
+        }
+
+        // Constructor
+        public AnimatedSprite(Texture2D texture, int numberOfColumns, int numberOfRows, int animationDelay, bool continuous)
+        {
+            this.Texture = texture;
+            this.columns = numberOfColumns;
+            this.rows = numberOfRows;
+            this.AnimationDelay = animationDelay;
+            this.continuous = continuous;
+            this.currentColumn = 0;
+            this.currentRow = 0;
+            this.frameWidth = texture.Width / columns; // One frame is the entire width of the texture divided by the number of columns
+            this.frameHeight = texture.Height / rows; // One frame is the entire height of the texture divided by the number of rows
+            this.animationTimer = 0;
+            this.complete = false;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 destination, SpriteEffects spriteEffect)
+        {
+            // If the animation is not complete (only for non-continous animations) then draw it
+            if (this.complete == false)
+            {
+                // Get a source rectangle representing the current frame in the texture
+                Rectangle frame = new Rectangle(currentColumn * frameWidth, currentRow * frameHeight, frameWidth, frameHeight);
+
+                // Draw the texture using only the part defined by the frame rectangle
+                spriteBatch.Draw(this.Texture, destination, frame, Color.White, 0, Vector2.Zero, 1,  spriteEffect, 1);
+            }
+
+            // Updating frame logic
+
+            if (this.animationTimer == 0) // If the animation timer is back on zero (has gone one whole turn), then change the frame
+            {
+                if (this.currentColumn < this.columns - 1) // If the current frame is not the in the last column...
+                {
+                    this.currentColumn++; // ...move forward one column
+                }
+                else // If it is the last column...
+                {
+                    if (this.currentRow < this.rows - 1)  // If the current frame is not the in the last row...
+                    {
+                        this.currentRow++;
+                        this.currentColumn = 0;
+                    }
+                    else if (this.continuous) // ...check if the animated sprite is a continuous animation and if so...
+                    {
+                        this.currentRow = 0;
+                        this.currentColumn = 0;
+                    }
+                    else // If it is not a continuous animation...
+                    {
+                        this.complete = true; // ...then set complete to true, indicating that the animation will not be drawn anymore
+                    }
+                }
+                // In any case, if the animation timer was at zero, add the animation delay to it, thus making it count down until the next update
+                this.animationTimer = this.AnimationDelay;
+            }
+            else // If the animation timer is not yet at zero, then no frame changing should be made and instead...
+            {
+                this.animationTimer--; // ...we just lower the timer by one
+            }
+        }
+    }
+}
--- JG0NE's Geim/FinalP_JOHAN_HUANG/FinalP_JOHAN_HUANG/FinalP_JOHAN_HUANG/Game1.cs
+++ JG0NE's Geim/FinalP_JOHAN_HUANG/FinalP_JOHAN_HUANG/FinalP_JOHAN_HUANG/Game1.cs
@@ -20,17 +20,53 @@ namespace FinalP_JOHAN_HUANG
         SpriteBatch spriteBatch;
         //The texture of the player
         Texture2D playerTexture;
+        Texture2D playerShadow;
         //The position of the player
         Vector2 playerPosition = new Vector2(100, 100);
+        //Player Rectangle
+        Rectangle playerRectangle;
+        //Shot Text
+        Texture2D shotTexture;
+        Texture2D shotShadow;
+        //Shot Position
+        List<Vector2> shotPositions;
+        //Shot Rectangles
+        Rectangle shotRectangles;
+        //Enemy textures
+        Texture2D enemyTexture1;
+        Texture2D enemyShadow1;
+        Texture2D enemyTexture2;
+        Texture2D enemyShadow2;
+        //Enemy Positions
+        List<Vector2> enemyPositions1;
+        List<Vector2> enemyPositions2;
+        //Enemy Rectangles
+        Rectangle enemyRectangles1;
+        Rectangle enemyRectangles2;
+        //Enemy Speed
+        List<double> enemySpeed1;
+        List<double> enemySpeed2;
+        //Enemy2 angle
+        List<float> enemyAngle;
         //Variable used to check the keyboardState
         KeyboardState keyboardState;
         //Variable used to check the previous keyboardState
         KeyboardState previousKeyboardState;
+        //Fonts
+        SpriteFont Font1;
+        //Random
+        Random RandomNumber = new Random();
+        //Timer Variable
+        float Timer;
+        float Timer2;
+        //Score Variable
+        int score;
+        //Kill count
+        int killCount;
         //Background Textures
         Texture2D nightSkyText;
         Texture2D cityAText;
         Texture2D cityBText;
-        //Texture2D nyanText;
         //Background Positions
         Vector2 BGnightSky1Pos;
         Vector2 BGnightSky2Pos;
@@ -38,13 +74,10 @@ namespace FinalP_JOHAN_HUANG
         Vector2 BGcityA2Pos;
         Vector2 BGcityB1Pos;
         Vector2 BGcityB2Pos;
-        //Vector2 nyan1Pos;
-        //Vector2 nyan2Pos;
         //Background Speeds
         float scrollspeed1 = 0.25f;
         float scrollspeed2 = 2.0f;
         float scrollspeed3 = 3.0f;
-        //float scrollspeedNyan = 2.5f;
         enum GameState { Main, Game, Pause, GameOver }
         GameState gameState;
         public Game1()
@@ -65,7 +98,10 @@ namespace FinalP_JOHAN_HUANG
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            gameState = GameState.Main;
+            shotPositions = new List<Vector2>();
+            enemyPositions1 = new List<Vector2>();
+            enemySpeed1 = new List<double>();
             base.Initialize();
         }
 
@@ -79,11 +115,16 @@ namespace FinalP_JOHAN_HUANG
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            playerTexture = Content.Load<Texture2D>("nyancat");
+            playerTexture = Content.Load<Texture2D>("PlayerShip");
+            playerShadow = Content.Load<Texture2D>("PlayerShipShadow");
+            shotTexture = Content.Load<Texture2D>("shot");
+            shotShadow = Content.Load<Texture2D>("shotShadow");
+            enemyTexture1 = Content.Load<Texture2D>("enemy");
+            enemyShadow1 = Content.Load<Texture2D>("enemyShadow");
             nightSkyText = Content.Load<Texture2D>("BGnightSky");
             cityAText = Content.Load<Texture2D>("BGcityA");
             cityBText = Content.Load<Texture2D>("BGcityB");
-            //nyanText = Content.Load<Texture2D>("nyan");
+            Font1 = Content.Load<SpriteFont>("Font1");
             //Asign the background positions
             BGnightSky1Pos = Vector2.Zero;
             BGnightSky2Pos = new Vector2(nightSkyText.Width, 0);
@@ -91,8 +132,6 @@ namespace FinalP_JOHAN_HUANG
             BGcityA2Pos = new Vector2(cityAText.Width, 0);
             BGcityB1Pos = Vector2.Zero;
             BGcityB2Pos = new Vector2(cityBText.Width, 0);
-            //nyan1Pos = Vector2.Zero;
-            //nyan2Pos = new Vector2(nyanText.Width, 0);
         }
 
         /// <summary>
@@ -118,22 +157,6 @@ namespace FinalP_JOHAN_HUANG
             // TODO: Add your update logic here
             previousKeyboardState = keyboardState;
             keyboardState = Keyboard.GetState();
-            if (keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left))
-            {
-                playerPosition.X += -5;
-            }
-            if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right))
-            {
-                playerPosition.X += 5;
-            }
-            if (keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Up))
-            {
-                playerPosition.Y += -5;
-            }
-            if (keyboardState.IsKeyDown(Keys.S) || keyboardState.IsKeyDown(Keys.Down))
-            {
-                playerPosition.Y += 5;
-            }
             switch (gameState)
             {
                 case GameState.Main:
@@ -143,13 +166,112 @@ namespace FinalP_JOHAN_HUANG
                     }
                     break;
                 case GameState.Game:
-                    if (keyboardState.IsKeyDown(Keys.P))
+                    if (keyboardState.IsKeyDown(Keys.Escape) && previousKeyboardState.IsKeyUp(Keys.Escape))
                     {
                         gameState = GameState.Pause;
                     }
+                    if (keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left))
+                    {
+                        playerPosition.X += -3;
+                    }
+                    if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right))
+                    {
+                        playerPosition.X += 7;
+                    }
+                    if (keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Up))
+                    {
+                        playerPosition.Y += -5;
+                    }
+                    if (keyboardState.IsKeyDown(Keys.S) || keyboardState.IsKeyDown(Keys.Down))
+                    {
+                        playerPosition.Y += 5;
+                    }
+                    if (keyboardState.IsKeyDown(Keys.Space) && previousKeyboardState.IsKeyUp(Keys.Space))
+                    {
+                        shotPositions.Add(new Vector2(playerPosition.X + playerTexture.Width - shotTexture.Width, playerPosition.Y + (playerTexture.Height / 2) - (shotTexture.Height / 2)));
+                    }
+                    //Update Rectangles
+                    playerRectangle = new Rectangle((int)playerPosition.X, (int)playerPosition.Y, playerTexture.Width, playerTexture.Height);
+                    for (int i = 0; i < shotPositions.Count; i++)
+                    {
+                        shotRectangles = new Rectangle((int)shotPositions[i].X, (int)shotPositions[i].Y, shotTexture.Width, shotTexture.Height);
+                        for (int j = 0; j < enemyPositions1.Count; j++)
+                        {
+                            enemyRectangles1 = new Rectangle((int)enemyPositions1[j].X, (int)enemyPositions1[j].Y, enemyTexture1.Width, enemyTexture1.Height);
+                            //Check if shot and enemy collide
+                            if (shotRectangles.Intersects(enemyRectangles1))
+                            {
+                                score += 100;
+                                killCount++;
+                                shotPositions.RemoveAt(i);
+                                enemyPositions1.RemoveAt(j);
+                            }
+                        }
+                    }
+
+                    //Move shots
+                    for (int i = 0; i < shotPositions.Count; i++)
+                    {
+                        shotPositions[i] += new Vector2(7, 0);
+                    }
+                    //Spawn Enemies
+                    Timer += 0.01f;
+                    Timer2 += 0.01f;
+                    if (killCount >= 20)
+                    {
+                        if (Timer2 >= 0.8f)
+                        {
+                            enemyPositions1.Add(new Vector2(GraphicsDevice.Viewport.Width + enemyTexture1.Width, RandomNumber.Next(0, GraphicsDevice.Viewport.Height - enemyTexture1.Height)));
+                            enemySpeed1.Add(RandomNumber.Next(5, 11));
+                            Timer2 = 0.0f;
+                        }
+                    }
+                    if (Timer >= 1.0f)
+                    {
+                        enemyPositions1.Add(new Vector2(GraphicsDevice.Viewport.Width + enemyTexture1.Width, RandomNumber.Next(0, GraphicsDevice.Viewport.Height - enemyTexture1.Height)));
+                        enemySpeed1.Add(RandomNumber.NextDouble() + RandomNumber.NextDouble() + RandomNumber.NextDouble() + RandomNumber.NextDouble() + RandomNumber.NextDouble() + RandomNumber.NextDouble() + RandomNumber.NextDouble() + RandomNumber.NextDouble() + RandomNumber.NextDouble() + RandomNumber.NextDouble());
+                        Timer = 0.0f;
+                    }
+                    //Move enemies
+                    for (int i = 0; i < enemyPositions1.Count; i++)
+                    {
+                        enemyPositions1[i] += new Vector2((float)-enemySpeed1[i], 0);
+                    }
+                    // Loop the backgrounds
+                    if (BGnightSky1Pos.X < -nightSkyText.Width)
+                    {
+                        BGnightSky1Pos.X = BGnightSky2Pos.X + nightSkyText.Width;
+                    }
+                    if (BGnightSky2Pos.X < -nightSkyText.Width)
+                    {
+                        BGnightSky2Pos.X = BGnightSky1Pos.X + nightSkyText.Width;
+                    }
+                    BGnightSky1Pos.X -= scrollspeed1;
+                    BGnightSky2Pos.X -= scrollspeed1;
+                    if (BGcityA1Pos.X < -cityAText.Width)
+                    {
+                        BGcityA1Pos.X = BGcityA2Pos.X + cityAText.Width;
+                    }
+                    if (BGcityA2Pos.X < -cityAText.Width)
+                    {
+                        BGcityA2Pos.X = BGcityA1Pos.X + cityAText.Width;
+                    }
+                    BGcityA1Pos.X -= scrollspeed2;
+                    BGcityA2Pos.X -= scrollspeed2;
+                    if (BGcityB1Pos.X < -cityBText.Width)
+                    {
+                        BGcityB1Pos.X = BGcityB2Pos.X + cityBText.Width;
+                    }
+                    if (BGcityB2Pos.X < -cityBText.Width)
+                    {
+                        BGcityB2Pos.X = BGcityB1Pos.X + cityBText.Width;
+                    }
+                    BGcityB1Pos.X -= scrollspeed3;
+                    BGcityB2Pos.X -= scrollspeed3;
+                    playerPosition.X -= scrollspeed2;
                     break;
                 case GameState.Pause:
-                    if (keyboardState.IsKeyDown(Keys.P))
+                    if (keyboardState.IsKeyDown(Keys.Escape) && previousKeyboardState.IsKeyUp(Keys.Escape))
                     {
                         gameState = GameState.Game;
                     }
@@ -161,47 +283,6 @@ namespace FinalP_JOHAN_HUANG
                     }
                     break;
             }
-            // Loop the backgrounds
-            if (BGnightSky1Pos.X < -nightSkyText.Width)
-            {
-                BGnightSky1Pos.X = BGnightSky2Pos.X + nightSkyText.Width;
-            }
-            if (BGnightSky2Pos.X < -nightSkyText.Width)
-            {
-                BGnightSky2Pos.X = BGnightSky1Pos.X + nightSkyText.Width;
-            }
-            BGnightSky1Pos.X -= scrollspeed1;
-            BGnightSky2Pos.X -= scrollspeed1;
-            if (BGcityA1Pos.X < -cityAText.Width)
-            {
-                BGcityA1Pos.X = BGcityA2Pos.X + cityAText.Width;
-            }
-            if (BGcityA2Pos.X < -cityAText.Width)
-            {
-                BGcityA2Pos.X = BGcityA1Pos.X + cityAText.Width;
-            }
-            BGcityA1Pos.X -= scrollspeed2;
-            BGcityA2Pos.X -= scrollspeed2;
-            if (BGcityB1Pos.X < -cityBText.Width)
-            {
-                BGcityB1Pos.X = BGcityB2Pos.X + cityBText.Width;
-            }
-            if (BGcityB2Pos.X < -cityBText.Width)
-            {
-                BGcityB2Pos.X = BGcityB1Pos.X + cityBText.Width;
-            }
-            BGcityB1Pos.X -= scrollspeed3;
-            BGcityB2Pos.X -= scrollspeed3;
-            //if (nyan1Pos.X > nyanText.Width)
-            //{
-            //    nyan1Pos.X = nyan2Pos.X - nyanText.Width;
-            //}
-            //if (nyan2Pos.X > nyanText.Width)
-            //{
-            //    nyan2Pos.X = nyan1Pos.X - nyanText.Width;
-            //}
-            //nyan1Pos.X += scrollspeedNyan;
-            //nyan2Pos.X += scrollspeedNyan;
             base.Update(gameTime);
         }
 
@@ -215,15 +296,42 @@ namespace FinalP_JOHAN_HUANG
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
-            spriteBatch.Draw(nightSkyText, BGnightSky1Pos, null, Color.White, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 1.0f);
-            spriteBatch.Draw(nightSkyText, BGnightSky2Pos, null, Color.White, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 1.0f);
-            //spriteBatch.Draw(nyanText, nyan1Pos, null, Color.White, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 1.0f);
-            //spriteBatch.Draw(nyanText, nyan2Pos, null, Color.White, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 1.0f);
-            spriteBatch.Draw(cityAText, BGcityA1Pos, null, Color.White, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 1.0f);
-            spriteBatch.Draw(cityAText, BGcityA2Pos, null, Color.White, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 1.0f);
-            spriteBatch.Draw(playerTexture, playerPosition, Color.White);
-            spriteBatch.Draw(cityBText, BGcityB1Pos, null, Color.White, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 1.0f);
-            spriteBatch.Draw(cityBText, BGcityB2Pos, null, Color.White, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 1.0f);
+            if (gameState == GameState.Main)
+            {
+                spriteBatch.DrawString(Font1, "Press Enter to Play", new Vector2(250, 300), Color.Red);
+            }
+            if (gameState == GameState.Game)
+            {
+                spriteBatch.Draw(nightSkyText, BGnightSky1Pos, null, Color.White, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 1.0f);
+                spriteBatch.Draw(nightSkyText, BGnightSky2Pos, null, Color.White, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 1.0f);
+                spriteBatch.Draw(cityAText, BGcityA1Pos, null, Color.White, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 1.0f);
+                spriteBatch.Draw(cityAText, BGcityA2Pos, null, Color.White, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 1.0f);
+                for (int i = 0; i < shotPositions.Count; i++)
+                {
+                    spriteBatch.Draw(shotTexture, shotPositions[i], Color.White);
+                }
+                for (int i = 0; i < enemyPositions1.Count; i++)
+                {
+                    spriteBatch.Draw(enemyTexture1, enemyPositions1[i], Color.White);
+                }
+                spriteBatch.Draw(playerTexture, playerPosition, Color.White);
+                spriteBatch.Draw(cityBText, BGcityB1Pos, null, Color.White, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 1.0f);
+                spriteBatch.Draw(cityBText, BGcityB2Pos, null, Color.White, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 1.0f);
+                spriteBatch.Draw(playerShadow, playerPosition, Color.White);
+                for (int i = 0; i < enemyPositions1.Count; i++)
+                {
+                    spriteBatch.Draw(enemyShadow1, enemyPositions1[i], Color.White);
+                }
+                for (int i = 0; i < shotPositions.Count; i++)
+                {
+                    spriteBatch.Draw(shotShadow, shotPositions[i], Color.White);
+                }
+                spriteBatch.DrawString(Font1, "Score: " + score, new Vector2(16, 16), Color.Red);
+            }
+            if (gameState == GameState.Pause)
+            {
+                spriteBatch.DrawString(Font1, "Press Esc to Continue Playing", new Vector2(200, 300), Color.Red);
+            }
             spriteBatch.End();
             base.Draw(gameTime);
         }
